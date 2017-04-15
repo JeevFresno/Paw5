964728ed4819aa865e98bc3b91d0b2f4117afc9b
FlossyM <flossymascarenhas@gmail.com>
2017-04-15 04:02:48 -0700

    Register Changes

---
+++ public/js/register.js
@@ -0,0 +1,37 @@
+/*$( document ).ready(function() {
+    console.log( "ready 123!" );
+
+    $.ajax({
+ 
+    // The URL for the request
+    url: "cvx",
+ 
+    // The data to send (will be converted to a query string)
+    data: {
+        id: 123
+    },
+ 
+    // Whether this is a POST or GET request
+    type: "GET",
+ 
+    // The type of data we expect back
+    dataType : "json",
+})
+  // Code to run if the request succeeds (is done);
+  // The response is passed to the function
+  .done(function( json ) {
+    
+  })
+  // Code to run if the request fails; the raw request and
+  // status codes are passed to the function
+  .fail(function() {
+    alert( "Sorry, there was a problem!" );
+   
+  })
+  // Code to run regardless of success or failure;
+  .always(function( xhr, status ) {
+    
+  });
+});
+
+*/
