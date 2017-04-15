3fd0b959cede9db8834df2ecfa945a2c2870c01c
Jeevjyot singh chhabda <jeevsingh@mail.fresnostate.edu>
2017-04-15 14:58:05 -0700

    SomeMoreImprovemenr

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,25 +1,40 @@
  $(document ).ready(function() {
 
-     alert(sessionStorage.email);
+     //alert(sessionStorage.email);
      //Accessing the profile information
 
+     (function(){
 
-     //Accessing
- 	 $("#lstLogOut a").on('click',function(e){
+         $.get('/profile',{email:sessionStorage.email},function(response){
+             console.log(response);
+             document.getElementById("name").innerHTML = response[0].firstname+ "Profile's";
+             document.getElementById("first_name").innerHTML = response[0].firstname;
+             document.getElementById("last_name").innerHTML = response[0].lastname;
+             document.getElementById("email").innerHTML = response[0].email;
+             document.getElementById("major").innerHTML = response[0].major;
+             document.getElementById("minor").innerHTML = response[0].minor;
+         });
 
-     $.get('/logout',function(data){
+     })();
+});
 
 
-            });
-    });
 
-  /**  $.get('/booksold',function(data){
+
+ /*$("#lstLogOut a").on('click',function(e){
+
+  $.get('/logout',function(data){
+
+
+  });
+  });*/
+
+ /*  $.get('/booksold',function(data){
    	alert(data);
    })
 
   $.get('/bookuploaded',function(data){
    	alert(data);
    })
- 
-   */
-});
+
+  */
