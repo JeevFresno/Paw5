94729dc942e4ce58feaac9f351318ef8cdd8247b
Jeevjyot singh chhabda <jeevsingh@mail.fresnostate.edu>
2017-04-14 19:15:31 -0700

    First Commit

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
 /**
  * Created by Jeevjyot on 4/14/17.
  */
+
+
+var express = require('express');
+
+var app = express();
+
+
+app.get('/',function(req,res){
+    res.send('Hello World');
+})
+app.listen(3000,function(req,res){
+    console.log('Hello World');
+    //res.send('Hello World');
+})
