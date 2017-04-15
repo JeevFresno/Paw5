ece0c1d91e69ab4451d473f91e087afb6fcd0bea
FlossyM <flossymascarenhas@gmail.com>
2017-04-15 12:18:50 -0700

    Book Listing

--- public/js/buy.js
+++ public/js/buy.js
@@ -1,9 +1,23 @@
 
 $(document).ready(function(){
 
-	 var textValue = document.getElementById('booklistingsearch').value;
 
-	 
+    $("#srchBtn").click(function(){
+     var textValue = document.getElementById('booklistingsearch').value;
+     var updateInfo ="";
+     var url ='https://www.googleapis.com/books/v1/volumes?q='+textValue;
+     console.log(url);
+     $.get(url,function(response){
+     	for (var i = 0; i < response.items.length; i++) {
+                var item = response.items[i];
+                 updateInfo=updateInfo+"<div class='media'><div class='media-left media-middle'><a href='#''><img class='media-object' src="+item.volumeInfo.imageLinks.smallThumbnail+"/></a></div><div class='media-body'><h4 class='media-heading'>"+item.volumeInfo.title+"</h4><h6 class='media-subheading'>by:"+item.volumeInfo.authors+" </h6></div></div>";
+
+            }
        
 
- });
+	 $('#updateContent').html(updateInfo);
+     });
+     });
+    });
+	
+
