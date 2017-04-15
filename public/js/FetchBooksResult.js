c7f9ed0a94d9990750b7c1b77558fde3f3243c37
Jeevjyot singh chhabda <jeevsingh@mail.fresnostate.edu>
2017-04-15 05:09:47 -0700

    UploadFunction

---
+++ public/js/FetchBooksResult.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Jeevjyot on 4/15/17.
+ */
+
+
+$(document).ready(function(){
+
+    $('#search').click(function(e){
+        e.preventDefault();
+       // alert('I am clicked');
+        var textValue = document.getElementById('textSearch').value;
+       // alert(textValue);
+        $.get('https://www.googleapis.com/books/v1/volumes?q='+textValue ,function(response){
+            for (var i = 0; i < response.items.length; i++) {
+                var item = response.items[i];
+                // in production code, item.text should have the HTML entities escaped.
+                console.log(item.volumeInfo.imageLinks.smallThumbnail);
+                document.getElementById("content").innerHTML += "<br>" + item.volumeInfo.title+" Author:"+item.volumeInfo.authors+" Publisher:"+item.volumeInfo.publisher+" :ISBN"+item.volumeInfo.industryIdentifiers[0].identifier+"<img src= "+item.volumeInfo.imageLinks.smallThumbnail+"  >";
+            }
+        })
+    });
+
+})
